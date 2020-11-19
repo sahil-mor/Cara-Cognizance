cadf114a9cb00cbbae4c1caff7668eb377276a72
Sahil Mor <iconsahil7@gmail.com>
2020-11-19 15:06:07 +0530

    removing compulsion of image for search

--- routeModels/index/searchItem.js
+++ routeModels/index/searchItem.js
@@ -6,20 +6,11 @@ const imageLink = require("../imagesLink/imageLink")
 const searchItem = async (req,res) => {
     try{
         var user = await User.findById(req.user.id)
-        if(!user.isAuthorised){
-            if( user.image.cloudinary_id == "ppupkcvzznejm0c6tylm" ){
-                req.flash("error","First you need to upload your image !!!")
-                res.redirect("/index")
-            }else{
-                var imagesLinks = await ImageLink.find({ imageUrl : { $ne : "https://res.cloudinary.com/dl3mvgfqz/image/upload/v1605605730/ppupkcvzznejm0c6tylm.png" } })
-                
-                res.render("searchItem",{ title : "Search",user, imagesLinks })
-            }
-        }else{
-            var imagesLinks = await ImageLink.find({ imageUrl : { $ne : "https://res.cloudinary.com/dl3mvgfqz/image/upload/v1605605730/ppupkcvzznejm0c6tylm.png" } })
-            
-            res.render("searchItem",{ title : "Search",user, imagesLinks })
-        }
+        
+        var imagesLinks = await ImageLink.find({ imageUrl : { $ne : "https://res.cloudinary.com/dl3mvgfqz/image/upload/v1605605730/ppupkcvzznejm0c6tylm.png" } })
+        
+        res.render("searchItem",{ title : "Search",user, imagesLinks })
+        
 
     }
     catch(err){
