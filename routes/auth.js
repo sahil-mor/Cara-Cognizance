df9be5aa1f63af6ebbe84c59700e679315cc4bcf
Sahil Mor <iconsahil7@gmail.com>
2020-11-19 15:18:35 +0530

    removing flash message

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,6 @@ var middleware = require("../middleware")
 router.post("/login",passport.authenticate("local",{
     failureRedirect : "/wrongCredentials"
 }),middleware.isLoggedIn, (req,res) => {
-    req.flash("success","hello falsh message")
     res.redirect("/index")
 })
 
